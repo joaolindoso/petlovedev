1fd23ce23120d7bd9371e14985e38f9fe1c37fec
joao.lindoso <joao.lindoso@seati.ma.gov.br>
2025-06-03 16:01:57 -0300

    Primeiro envio de arquivos...

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,25 @@
+export function Hero() {
+  return (
+    <section className="bg-[#e84c3d] text-white relative overflow-hidden">
+
+      <div>
+        <article>
+          <div>
+            <h1>
+              Seu pet merece cuidado, carinho e atenção especial.
+            </h1>
+            <p> 
+              Oferecemos os melhores serviços para garantir o bem-estar e a felicidade do seu amigo de quatro patas.
+            </p>
+            <div>
+              <a href="#">
+                Contato via WhatsApp
+              </a>
+            </div>
+          </div>
+        </article>
+      </div>
+
+    </section> 
+  )
+}
